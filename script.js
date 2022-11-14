77a5525d1ef8b4bb728a0ee36f92c915202d084f
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-11-15 04:12:56 +0800

    Add id to div squares on gameboard

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ const gameBoard = document.getElementById("gameBoard");
 
 //create gameBoard
 function createGameBoard () {
-    for (let i = 0; i < 100; i ++) {
+    for (let i = 1; i < 101; i ++) {
         let div = document.createElement("div");
+        div.setAttribute("id",i);
+        div.innerHTML=i;
         gameBoard.appendChild(div);
         console.log(div);
+
     }
 }
 
 createGameBoard();
 
+//Function = Make soot ghost appear randomly on the grid
+let squares = document.querySelector(".gameBoard div");
+function randomSootGhost(squares) {
+    const sootGhost = document.createElement("img");
+    sootGhost.src = "Images/sootGhostIcon.gif";
+    gameBoard.appendChild(sootGhost);
 
+}
+
+randomSootGhost(squares);
