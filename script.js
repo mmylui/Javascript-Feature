d7fdf90b2e95abbec986c7a9b09964b1ababe687
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-11-15 09:55:26 +0800

    fixed bug, add randomDiv as function

--- script.js
+++ script.js
@@ -17,38 +17,44 @@ createGameBoard();
 //on click of soot ghost, score +10;
 //addeventlistener on sootghost image, on click, it will change score to score =+ 10;
 //change image of randomDiv to handprint when soot ghost image is clicked
-    const sootGhost = document.createElement("img");
-    sootGhost.src = "Images/sootGhostIcon.gif";
-    const handprint = document.createElement("img");
+const sootGhost = document.createElement("img");
+sootGhost.src = "Images/sootGhostIcon.gif";
+const handprint = document.createElement("img");
 handprint.src = "Images/handprint2.png";
 
-
-function startGame() {
-    let randomDiv = document.getElementById(Math.floor(Math.random()*100));
-    randomDiv.appendChild(sootGhost);
-    
-    function removeHandprint () {
-        randomDiv.removeChild(randomDiv.children[0]);
-    }
-    function addScore () {
-        console.log("click detected");
-        score += 10;
-        scoreText.innerHTML = score;
-        randomDiv.replaceChild(handprint,randomDiv.firstChild);
-        setTimeout(removeHandprint,500);
-    }
-    sootGhost.addEventListener("click", addScore);
-
+function randomDivID() {
+    let randomDivID = Math.floor(Math.random()*100);
+    return randomDivID;
 }
-//Every three seconds run startGame()
-    setInterval(startGame, 3000);
 
+function randomSootGhost() {
+    console.log(randomDivID());
+    let randomDiv = document.getElementById(randomDivID());
+    randomDiv.appendChild(sootGhost);
+}
+//Every three seconds randomSootGhost()
+    setInterval(randomSootGhost, 3000);
 //Add score board
 let scoreBoard = document.getElementById("scoreDisplay")
 let scoreText = document.createElement("div");
 let score = 0;
 scoreText.innerHTML = score;
 scoreBoard.appendChild(scoreText);
+
+function addScore () {
+console.log("click detected");
+score += 10;
+scoreText.innerHTML = score;
+let randomDiv = sootGhost.parentNode;
+randomDiv.replaceChild(handprint,randomDiv.firstChild);
+setTimeout(removeHandprint,500);
+}
+sootGhost.addEventListener("click", addScore);
+
+function removeHandprint () {
+let randomDiv = handprint.parentNode;
+randomDiv.removeChild(randomDiv.firstChild);
+}
 
 //set timer 30s, setInterval updating every second, timer starting at 30s, reducing by one at each second interval, when timer = 0, game over, popup score = score
 function timerDisplay() {
